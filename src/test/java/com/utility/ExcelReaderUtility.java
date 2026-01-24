8d43abe591fc4c75d0ddc4b6f043458e2b858422
Anand <anand.shopping@gmail.com>
2026-01-24 08:28:12 -0500

    adding xls utility file
    adding xls utility file

---
+++ src/test/java/com/utility/ExcelReaderUtility.java
@@ -0,0 +1,18 @@
+package com.utility;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+
+public class ExcelReaderUtility {
+
+public static void main(String[] args) {
+
+    File file=new File(System.getProperty("user.dir")+"/testData/LoginData.xlsx");
+
+    XSSFWorkbook xssfWorkbook=new XSSFWorkbook();
+
+}
+
+
+}
